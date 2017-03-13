ecfe3bb619b56c5174dffb0ae14c7dbffd0f55e5
jmbaluyut <jmbaluyut1@gmail.com>
2017-03-13 16:36:42 -0700

    added esj. pray for some visuals

--- server.js
+++ server.js
@@ -14,6 +14,26 @@ var path = require("path");
 var mime = require("mime");
 var exec = require("child_process").exec
 
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+
 //http.listen(8080, "127.0.0.1");
 
 io.on('connection', function (socket) {
@@ -86,10 +106,6 @@ var bttn4 = 0;
 var bttn5 = 0;
 var bttn6 = 0;
 
-var app = express();
-app.get("/", function (req, res) {
-    res.sendfile("./index.html");
-});
 
 io.sockets.on("connect", function (socket) {
     socket.emit("connection_made");
